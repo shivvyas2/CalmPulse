8484a5b5e4dcb5084ef5cef7cd578a01c9bd4218
Shiv Vyas <sv21670n@pace.edu>
2024-12-07 00:38:38 -0500

    Changes in toml files to add icons from Material UI

--- CalmPulse/app/build.gradle.kts
+++ CalmPulse/app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.leanback)
+    implementation (androidx.compose.material:material-icons-extended:1.5.1)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
